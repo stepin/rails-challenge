18d3752e3fd103d4697bad0297a3ba738ad9827d
Igor Stepin <igor@stepin.name>
2019-03-05 02:39:55 +0400

    more tests

--- test/controllers/v1/patients_controller_test.rb
+++ test/controllers/v1/patients_controller_test.rb
@@ -11,6 +11,68 @@ module V1
       response = JSON.parse(@response.body)
       assert_equal 2, response.length
       assert_not_nil response[0]["total_amount"]
+      assert 50 == response[0]["total_amount"] || 7 == response[0]["total_amount"]
+      assert 50 == response[1]["total_amount"] || 7 == response[1]["total_amount"]
+    end
+
+    test "get patients with payment_min (edge)" do
+      get v1_patients_path, params: { payment_min: 10}
+
+      assert_response :success
+
+      response = JSON.parse(@response.body)
+      assert_equal 2, response.length
+      assert_not_nil response[0]["total_amount"]
+      assert 50 == response[0]["total_amount"] || 0 == response[0]["total_amount"]
+      assert 50 == response[1]["total_amount"] || 0 == response[1]["total_amount"]
+    end
+
+    test "get patients with payment_min" do
+      get v1_patients_path, params: { payment_min: 11}
+
+      assert_response :success
+
+      response = JSON.parse(@response.body)
+      assert_equal 2, response.length
+      assert_not_nil response[0]["total_amount"]
+      assert 40 == response[0]["total_amount"] || 0 == response[0]["total_amount"]
+      assert 40 == response[1]["total_amount"] || 0 == response[1]["total_amount"]
+    end
+
+    test "get patients with payment_max (edge)" do
+      get v1_patients_path, params: { payment_max: 40}
+
+      assert_response :success
+
+      response = JSON.parse(@response.body)
+      assert_equal 2, response.length
+      assert_not_nil response[0]["total_amount"]
+      assert 50 == response[0]["total_amount"] || 7 == response[0]["total_amount"]
+      assert 50 == response[1]["total_amount"] || 7 == response[1]["total_amount"]
+    end
+
+    test "get patients with payment_max" do
+      get v1_patients_path, params: { payment_max: 39}
+
+      assert_response :success
+
+      response = JSON.parse(@response.body)
+      assert_equal 2, response.length
+      assert_not_nil response[0]["total_amount"]
+      assert 10 == response[0]["total_amount"] || 7 == response[0]["total_amount"]
+      assert 10 == response[1]["total_amount"] || 7 == response[1]["total_amount"]
+    end
+
+    test "get patients with payment_min and payment_max" do
+      get v1_patients_path, params: { payment_min: 20, payment_max: 30}
+
+      assert_response :success
+
+      response = JSON.parse(@response.body)
+      assert_equal 2, response.length
+      assert_not_nil response[0]["total_amount"]
+      assert 0 == response[0]["total_amount"]
+      assert 0 == response[1]["total_amount"]
     end
 
     test "simple import patients" do
