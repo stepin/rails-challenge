de071322ad552cdca39674a68eeb8d53710aba9c
Igor Stepin <igor@stepin.name>
2019-03-04 23:05:04 +0400

    first test

--- test/controllers/v1/patients_controller_test.rb
+++ test/controllers/v1/patients_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
-class PatientsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+module V1
+  class PatientsControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+
+    test "simple get patients" do
+      get v1_patients_path
+
+      assert_response :success
+
+      response = JSON.parse(@response.body)
+      assert 0 == response.length
+    end
+  end
 end
--- test/controllers/v1/payments_controller_test.rb
+++ test/controllers/v1/payments_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
-class PaymentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+module V1
+  class PaymentsControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
 end
