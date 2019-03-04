c4e7d0c382b1d5e1a6c0309022187dc2ba6a7c07
Igor Stepin <igor@stepin.name>
2019-03-04 12:30:34 +0400

    Controllers stubs

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,13 @@
 class PaymentsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    render json: "ok"
+  end
+
+  def import
+    render json: "ok"
+  end
+
+  private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/patients', to: 'patients#show'
+  get '/patients', to: 'patients#index'
   post '/patients', to: 'patients#import'
 
-  get '/payments', to: 'payments#show'
+  get '/payments', to: 'payments#index'
   post '/payments', to: 'payments#import'
 end
