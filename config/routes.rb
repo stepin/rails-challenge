0de26e37ea16d8307cd9e03845697fdc5721bf45
Igor Stepin <igor@stepin.name>
2019-03-04 12:10:32 +0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :payments
-  resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/patients', to: 'patients#show'
+  post '/patients', to: 'patients#import'
+
+  get '/payments', to: 'payments#show'
+  post '/payments', to: 'payments#import'
 end
